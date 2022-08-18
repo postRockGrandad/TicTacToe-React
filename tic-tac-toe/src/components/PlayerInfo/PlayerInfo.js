01f701795f7fadf39a1212000c33ed6fcf1dd893
Richard-PC\Richard <richgibson123@hotmail.co.uk>
2022-08-18 21:06:33 +0100

    initial changes for player-name input. change board viewmodel to be 2d array.

---
+++ tic-tac-toe/src/components/PlayerInfo/PlayerInfo.js
@@ -0,0 +1,33 @@
+import './PlayerInfo.css'
+
+export function PlayerInfoModal(props) {  
+    return (
+        <div className="modalBackground">
+            <div className="modalContainer">
+            <div className="title">
+                <h1>Tic Tac Toe with React Hooks</h1>
+            </div>
+            <div className="body">
+                <p>Enter player names:</p>
+                <div className="input-container">
+                <label htmlFor="player1Name">Player 1:</label>
+                <input id="player1Name" className="player1" onChange={(event)=>{props.playerNameChange(0, event.target.value);}}/>
+                </div>
+                <div className="input-container">
+                    <label htmlFor="player2Name">Player 2:</label>
+                    <input id="player2Name" className="player2" onChange={(event)=>{props.playerNameChange(1, event.target.value);}}/>
+                </div>
+            </div>
+            <div className="footer">
+                <button id="playBtn"
+                    onClick={() => {
+                        props.startGame()
+                    }}
+                >
+                    Play
+                </button>
+            </div>
+            </div>
+        </div>
+    );
+}
--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -1,20 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { PlayerInfoModal } from './components/PlayerInfo/PlayerInfo';
 import './index.css';
-
-//controlled function component
-//- maintains no state
-//- impl as the return of the would-be render() function
-function Square(props) {
-  return (
-    <button 
-      className="square" 
-      onClick={()=>{props.squareClickCallback()}}
-    >
-      {props.value}
-    </button>
-  );
-}
 
 class Board extends React.Component {
   constructor(props){
@@ -23,54 +10,51 @@ class Board extends React.Component {
     this.state = {
       currentValue: 'X',
       winner: null,
-      squareValues: Array(9).fill(null)
+      squareValues: Array.from(
+        { length:3 }, () => (Array.from({ length:3 }, ()=> null))
+      )
     };
   }
 
-  selectSquare(i) {
+  selectSquare(row, col) {
     const squareValues = this.state.squareValues.slice();
     const winner = this.state.winner;
-    if(squareValues[i] === null && !winner){
+
+    if(squareValues[row][col] === null && !winner){
       const nextValue = this.state.currentValue === 'X' ? 'O' : 'X';
 
-      squareValues[i] = this.state.currentValue;
+      squareValues[row][col] = this.state.currentValue;
       this.setState({squareValues: squareValues, currentValue: nextValue});
     } else {
-      //TODO: error that square has already been used in previous turn
+      //TODO: error that square has already been used in previous turn or winner exists
     }
   }
 
-  calculateWinner() {
-    const squareValues = this.state.squareValues.slice();
+  determineWinningValue() {
+    const flatIndexValues = this.state.squareValues.slice().flat(1);
 
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
+    const winConditionIndexes = [
+      [0, 1, 2], // horizontal (1st row) 
+      [3, 4, 5], // horizontal (2nd row)
+      [6, 7, 8], // horizontal (3rd row)
+      [0, 3, 6], // vertical (1st column)
+      [1, 4, 7], // vertical (2nd column)
+      [2, 5, 8], // vertical (3rd column)
+      [0, 4, 8], // diagonal (top-left -> bottom-right)
+      [2, 4, 6], // diagonal (bottom-left -> top-right)
     ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squareValues[a] && squareValues[a] === squareValues[b] && squareValues[a] === squareValues[c]) {
-        return squareValues[a];
+    for (let i = 0; i < winConditionIndexes.length; i++) {
+      const [a, b, c] = winConditionIndexes[i];
+      //victory if value at all indexes for given win condition are equal and not nullish
+      if (flatIndexValues[a] && flatIndexValues[a] === flatIndexValues[b] && flatIndexValues[a] === flatIndexValues[c]) {
+        return flatIndexValues[a];
       }
     }
     return null;
   }
 
-  renderSquare(i) {
-    return <Square 
-      value={this.state.squareValues[i]}
-      squareClickCallback={() => this.selectSquare(i)}
-    />;
-  }
-
   render() {
-    let winner = this.calculateWinner();
+    let winner = this.determineWinningValue();
     let player = this.state.currentValue ==='X' ? this.props.playerNames[0] : this.props.playerNames[1];
     let message = winner ? "Winner is: " : "Next player is: ";
 
@@ -79,43 +63,72 @@ class Board extends React.Component {
     return (
       <div>
         <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        <table>
+          <tbody>
+            {/* 3 rows of 3 cells */}
+            {[...Array(3).keys()].map(row => 
+            <tr key={row}> 
+              {[...Array(3).keys()].map(col => 
+              <td onClick={()=>{this.selectSquare(row, col)}} key={row + "_" + col}> 
+                {this.state.squareValues[row][col]}
+              </td>)} 
+            </tr>)}
+          </tbody>
+        </table>
       </div>
     );
   }
 }
 
+
+//========================================================================================================================
+
 class Game extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      promptPlayerNames: true,
+      players: {0: null, 1: null}
+    }
+  }
+
+  setPlayer(key, value){
+    let players = this.state.players;
+
+    players[key] = value;
+    this.setState({players});
+  }
+
+  startGame(){
+    //if both players have names given, hide input modal and begin game
+    if(this.state.players['0'] && this.state.players['1']){
+      this.setState({...this.state, promptPlayerNames: false});
+    } else {
+      //TODO throw error about missing player names
+    }
+  }
+
   render() {
     return (
       <div className="game">
         <div className="game-board">
-          <Board playerNames={['John', 'David']}/>
+          <Board playerNames={this.state.players}/>
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
         </div>
+
+        {this.state.promptPlayerNames && <PlayerInfoModal playerNameChange={(playerName, value) => this.setPlayer(playerName, value)} startGame={()=> this.startGame()}/>}
       </div>
     );
   }
 }
 
-// ========================================
+
+//========================================================================================================================
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
